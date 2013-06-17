17085005353e14afac87e020eb9d70631f50b590
Sejr Andersen <sejr.andersen@gmail.com>
2013-06-16 19:03:55 -0700

    Update LongestTwoCharacters

--- Challenges/src/LongestTwoCharacters.java
+++ Challenges/src/LongestTwoCharacters.java
@@ -4,12 +4,26 @@
  *
  * Simply print the longest sub-string of the given string that contains, at most, 
  * two unique characters. If you find multiple sub-strings that match the 
- * description, print the last sub-string (furthest to the right).
+ * description, print the last sub-string (farthest to the right).
  */
 
 
 public class LongestTwoCharacters {
 
+	/**
+	 * Verify that the string contains only the same character.
+	 * @param longestCurrent
+	 * @return boolean
+	 */
+	public static boolean verifyString(String longestCurrent) {
+		char[] chars = longestCurrent.toCharArray();
+		for(int i=0; i < chars.length - 1; i++) {
+			if (chars[i] != chars[i+1]) 
+				return false;
+		}
+		return true;
+	}
+	
 	
 	public static String subString(String str) {
 		String firstChar = str.substring(0,1);
@@ -19,13 +33,12 @@ public class LongestTwoCharacters {
 		
 		for(int i=2; i<str.length(); i++) {
 			String substring = str.substring(i,i+1);
-			
-			if (longestCurrent.contains(substring)) {
+			 if (longestCurrent.contains(substring) || verifyString(longestCurrent)) {
 				longestCurrent = longestCurrent + substring;
 			} else {
 				if (longestCurrent.length() >= longestArchive.length())
 				  longestArchive = longestCurrent;
-				longestCurrent = longestCurrent.substring(longestCurrent.length() - 1 ) + substring;
+				longestCurrent = longestCurrent.substring(longestCurrent.length() -1 ) + substring;
 			}
 			
 		}
@@ -43,14 +56,16 @@ public class LongestTwoCharacters {
 	 */
 	public static void main(String[] args) {
 
-		String str  = "abbccc";   // bbccc
+		String str  = "aaabbccc";   // bbccc
 		String str2 = "abcaaabcabcabccc"; // bccc
 		String str3 = "qwertyytrewq"; // tyyt
 		
 		
 		
 		
-		System.out.println(subString(str2));
+		System.out.println(subString(str));
+//		System.out.println(subString(str2));
+//		System.out.println(subString(str3));
 		
 	}
 
